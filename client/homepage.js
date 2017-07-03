d5b0cddcaa9b62f8b1e98e50c66cf369dd33a6fa
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-07-03 01:16:07 -0400

    link buttons

---
+++ client/homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import particlesJS from 'particlesjs';
+
+
+export default class HOMEPAGE extends Component {
+  render() {
+    return (
+      <div>
+        HomePage
+      </div>
+    )
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const pdf = require('express-pdf');
+
+app.use(pdf);
+app.use(express.static(path.join(__dirname, '/../client')));
+app.get('/resume', (req,res)=> {
+  res.pdf(path.resolve(__dirname, './Resume.pdf'));
+})
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname, '/../client/index.html'))
+})
+let port = 5000;
+app.listen(port,()=>{
+  console.log("Listening on port " + port);
+});
+
+module.exports = app;
